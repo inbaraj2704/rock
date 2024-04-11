84de9a54d01e5d0fe245d271f96abf83b9cad0f5
inbaraj2704 <166735428+inbaraj2704@users.noreply.github.com>
2024-04-11 21:14:26 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+
+    return 
+    <div className="msd">
+    <link to="/">Home</link>
+    </div>
+    
+} ;
+
+export default Home ;
